80cd384c6ecb1ae994f9f89e4c16a4ac3a8c9994
DYNMC <o.a.zhydenko.fitis24@chdtu.edu.ua>
2026-06-23 22:12:47 +0300

    Add files via upload

---
+++ Lab2_var1.5.py
@@ -0,0 +1,14 @@
+import random
+
+# Генерація масиву з 20 випадкових цілих чисел у діапазоні від 1 до 100
+arr = [random.randint(1, 100) for _ in range(20)]
+print("Згенерований масив:", arr)
+
+# Вибір чисел, кратних 11
+multiples = [x for x in arr if x % 11 == 0]
+
+# Сортування у порядку спадання
+multiples.sort(reverse=True)
+
+# Вивід результату
+print("Числа кратні 11 у порядку спадання:", multiples)
---
+++ Lab2_var2.5.py
@@ -0,0 +1,27 @@
+# Введення розмірів матриці
+m = int(input("Введіть кількість рядків m: "))
+n = int(input("Введіть кількість стовпців n: "))
+
+# Введення чисел через пропуск
+numbers = input("Введіть цілі числа через пробіл: ").split()
+
+# Перевірка коректності введення
+if len(numbers) != m * n:
+    print("Помилка: кількість чисел не відповідає розмірам матриці!")
+else:
+    # Перетворення у двовимірний список
+    matrix = []
+    for i in range(m):
+        row = [int(numbers[i*n + j]) for j in range(n)]
+        matrix.append(row)
+
+    print("Початкова матриця:")
+    for row in matrix:
+        print(row)
+
+    # Відображення відносно горизонтальної осі
+    flipped = matrix[::-1]
+
+    print("Матриця після відображення:")
+    for row in flipped:
+        print(row)
